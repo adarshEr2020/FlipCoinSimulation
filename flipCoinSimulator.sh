fe848389cf2e0216fa6dbe8c2aee9270b436101c
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-06 09:57:32 +0530

    Add : how many times win head and tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
-echo "Flip Coin Simulation"
-echo  " This problem displays the winner Heads or Tails"
-heads=1
-flip=$((RANDOM%2))
-if [ $flip -eq $heads ]
-then 
-	echo "Flip coin : Heads"
-else
-	echo "Flip coins : Tails"
-fi
+echo "Flip Coin Simulation problem displays the winner Heads or Tails"
+heads=0
+tails=0
+read -p "Enter number of flip coin required" n
+for((i=0; i<n; i++))
+do
+	flip=$((RANDOM%2))
+	if [ $flip -eq 1 ]
+	then
+		heads=$(($heads+1))
+	else
+		tails=$(($tails+1))
+	fi
+done
+echo "number of times head win" $heads
+echo "Number of times tails win" $tails
