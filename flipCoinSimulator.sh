000d18bd61f6d2f83be9d7553ae3c1f25e5758ed
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-06 09:21:30 +0530

    Add : flip coin result

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 echo "Flip Coin Simulation"
 echo  " This problem displays the winner Heads or Tails"
+heads=1
+flip=$((RANDOM%2))
+if [ $flip -eq $heads ]
+then 
+	echo "Flip coin : Heads"
+else
+	echo "Flip coins : Tails"
+fi
