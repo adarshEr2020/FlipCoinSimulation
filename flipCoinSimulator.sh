2944b5212286f9f601ab163713f77f379c4098b9
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-06 11:17:05 +0530

    Add : flip coin Extends 21 time head or tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,8 +2,7 @@
 echo "Flip Coin Simulation problem displays the winner Heads or Tails"
 heads=0
 tails=0
-read -p "Enter number of flip coin required" n
-for((i=0; i<n; i++))
+while [ $heads -lt 21 ] && [ $tails -lt 21 ]
 do
 	flip=$((RANDOM%2))
 	if [ $flip -eq 1 ]
@@ -13,5 +12,12 @@ do
 		tails=$(($tails+1))
 	fi
 done
-echo "number of times head win" $heads
-echo "Number of times tails win" $tails
+if [ $heads -eq 21 ] && [ $tails -eq 21 ]
+then
+	echo "Its a Tie"
+elif [ $heads -eq 21 ]
+then
+	echo "Head won by" $(($heads-$tails))
+else
+	echo "Tail Won by" $(($tails-$heads))
+fi
